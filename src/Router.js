2260af26b06a834261226d32e9e07a543091b31e
Uğur Bilgin <35350117+ugurthedeveloper@users.noreply.github.com>
2020-10-04 05:47:01 +0300

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Day1_2 from "./Pages/Day1/Day-1.2";
 import Day1_1 from "./Pages/Day1/Day-1.1";
 import Day2 from "./Pages/Day2";
+import Day3 from "./Pages/Day3";
+import Day4 from "./Pages/Day4";
+
 
 export default function Router() {
   return (
@@ -10,6 +13,9 @@ export default function Router() {
       <Route path="/day-1.1" component={Day1_1} />
       <Route path="/day-1.2" component={Day1_2} />
       <Route path="/day-2" component={Day2} />
+      <Route path="/day-3" component={Day3} />
+      <Route path="/day-4" component={Day4} />
     </BrowserRouter>
   );
 }
+
